9b0fce999d3640ed1fa60d68cccf51f62b6ed29d
AhmedEHaddad <ahmed.e.h.2.55@gmail.com>
2022-09-03 02:56:36 +0300

    connect API to clientside

--- website/app.js
+++ website/app.js
@@ -1,6 +1,16 @@
 /* Global Variables */
 
+// HTML element to listen for click events
+const button = document.getElementById('generate')
 
+// HTML elements to get the values
+const zip = document.getElementById('zip')
+const feelings = document.getElementById('feelings')
+
+// HTML elements to update dynamically
+const date = document.getElementById('date')
+const temp = document.getElementById('temp')
+const content = document.getElementById('content')
 
 
 //OpenWeatherMap API key
@@ -12,3 +22,81 @@ const apiKey = 'cb1bcbb049c4632e066ef5d9f01d6e53';
 // Create a new date instance dynamically with JS
 let d = new Date();
 let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+
+/* add functionallity to generate button */
+// add eventlistener to button
+document.getElementById('generate').addEventListener('click', performAction);
+// function
+function performAction(e) {
+    e.preventDefault();
+    // get user input values
+    const newZip = document.getElementById('zip').value;
+    const content = document.getElementById('feelings').value;
+  
+    getWeatherInfo(newZip)
+      .then(function (userData) {
+        // add data to POST request
+        postData('/api', { date: newDate, temp: userData.main.temp, content })
+      }).then(function (newData) {
+        // update browser content
+        updateUI()
+      })
+    // reset form
+    form.reset();
+  }
+
+/* get route */
+// GET weather API data
+const getWeatherInfo = async (zip) => {
+await fetch(`${baseUrl}?zip=${zip}&units=imperial&APPID=${apiKey}`);
+try {
+    // result of fetch function
+    const userData = await res.json();
+    return userData;
+  } catch (error) {
+    console.log("error", error);
+  }
+
+}
+
+/* Post route client side*/
+const postData = async ( url = '', data = {})=>{
+    console.log(data);
+      const response = await fetch(url, {
+      method: 'POST', 
+      credentials: 'same-origin',
+      headers: {
+          'Content-Type': 'application/json',
+      },
+     // Body data type must match "Content-Type" header        
+      //body: JSON.stringify(data), 
+      body: JSON.stringify({
+        date: data.date,
+        temp: data.temp,
+        content: data.content
+      }),
+    });
+
+      try {
+        const newData = await response.json();
+        console.log(newData);
+        return newData;
+      }catch(error) {
+      console.log("error", error);
+      }
+  }
+  /* dynamically update UI */
+  const updateUI = async () => {
+    const request = await fetch('/api');
+    try {
+      const allData = await request.json()
+      
+      // update new  values
+      document.getElementById('date').innerHTML = allData.date;
+      document.getElementById('temp').innerHTML = allData.temp;
+      document.getElementById('content').innerHTML = allData.content;
+    }
+    catch (error) {
+      console.log("error", error);
+    }
+  };
